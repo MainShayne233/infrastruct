828755b761a513d7a5e3867feded81293e798fb6
MainShayne233 <shaynetremblay@hotmail.com>
2018-05-26 14:21:49 -0400

    Base function setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 // @flow
 
-/*
- * Doubles the number passed to it
- * @example
- * double(1.2)
- * //=> 2.4
+/**
+ * TODO write docs
  */
-export const double = (x: number): number => 2 * x;
-
-/*
- * Double doubles the number passed to it
- * @example
- * doubleDouble(1.2)
- * //=> 4.8
- */
-export const doubleDouble = (x: number): number => double(double(x));
+export default (): ((Object) => Object) => (params: Object) => ({ ...params });
--- test/index_test.js
+++ test/index_test.js
@@ -1,20 +1,17 @@
 /* globals it describe */
 import doctest from 'jsdoc-test';
 import { expect } from 'chai';
-import { double, doubleDouble } from '../src';
+import defineStruct from '../src';
+
+const SAMPLE_PROPERTIES = ['name', 'age', 'zodiac'];
 
 describe('doctests', () => {
   doctest('./src/index.js');
 });
 
-describe('double', () => {
-  it('returns double the number passed in', () => {
-    expect(double(3)).to.equal(6);
-  });
-});
-
-describe('doubleDouble', () => {
-  it('returns quadruple the number passed in', () => {
-    expect(doubleDouble(3)).to.equal(12);
+describe('makeStruct', () => {
+  it('should return a function that will produce structs', () => {
+    const structCreator = defineStruct();
+    expect(structCreator).to.be.a('function');
   });
 });
